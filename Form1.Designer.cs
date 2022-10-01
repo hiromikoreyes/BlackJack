700ec6a278a66de1c28f32998b99e7a806d30abb
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-09-30 21:06:01 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.lblDealerCards = new System.Windows.Forms.Label();
+            this.lblPlayerCards = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlayerValue = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHit
+            // 
+            this.btnHit.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnHit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHit.Enabled = false;
+            this.btnHit.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnHit.Location = new System.Drawing.Point(43, 364);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(124, 60);
+            this.btnHit.TabIndex = 0;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = false;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnStand.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStand.Enabled = false;
+            this.btnStand.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStand.Location = new System.Drawing.Point(173, 364);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(124, 60);
+            this.btnStand.TabIndex = 1;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = false;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.Location = new System.Drawing.Point(43, 335);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(360, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnDeal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDeal.Enabled = false;
+            this.btnDeal.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDeal.Location = new System.Drawing.Point(303, 364);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(100, 60);
+            this.btnDeal.TabIndex = 4;
+            this.btnDeal.Text = "Deal";
+            this.btnDeal.UseVisualStyleBackColor = false;
+            this.btnDeal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDealerCards
+            // 
+            this.lblDealerCards.AutoSize = true;
+            this.lblDealerCards.Font = new System.Drawing.Font("Ink Free", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblDealerCards.ForeColor = System.Drawing.Color.Transparent;
+            this.lblDealerCards.Location = new System.Drawing.Point(39, 29);
+            this.lblDealerCards.Name = "lblDealerCards";
+            this.lblDealerCards.Size = new System.Drawing.Size(170, 26);
+            this.lblDealerCards.TabIndex = 5;
+            this.lblDealerCards.Text = "DEALER CARDS";
+            // 
+            // lblPlayerCards
+            // 
+            this.lblPlayerCards.AutoSize = true;
+            this.lblPlayerCards.Font = new System.Drawing.Font("Ink Free", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblPlayerCards.ForeColor = System.Drawing.Color.Transparent;
+            this.lblPlayerCards.Location = new System.Drawing.Point(39, 167);
+            this.lblPlayerCards.Name = "lblPlayerCards";
+            this.lblPlayerCards.Size = new System.Drawing.Size(166, 26);
+            this.lblPlayerCards.TabIndex = 6;
+            this.lblPlayerCards.Text = "PLAYER CARDS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(458, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 52);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "BLACKJACK    \r\n                Miko Reyes (2022)\r\n";
+            // 
+            // lblPlayerValue
+            // 
+            this.lblPlayerValue.AutoSize = true;
+            this.lblPlayerValue.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPlayerValue.ForeColor = System.Drawing.Color.Transparent;
+            this.lblPlayerValue.Location = new System.Drawing.Point(43, 294);
+            this.lblPlayerValue.Name = "lblPlayerValue";
+            this.lblPlayerValue.Size = new System.Drawing.Size(127, 23);
+            this.lblPlayerValue.TabIndex = 9;
+            this.lblPlayerValue.Text = "Player Value: 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BlackJack.Properties.Resources.boredape1;
+            this.pictureBox1.Location = new System.Drawing.Point(472, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblPlayerValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlayerCards);
+            this.Controls.Add(this.lblDealerCards);
+            this.Controls.Add(this.btnDeal);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Name = "Form1";
+            this.Text = "Black Jack!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Label lblDealerCards;
+        private System.Windows.Forms.Label lblPlayerCards;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlayerValue;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,585 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    //Miko Reyes
+    //June 2022
+
+    public partial class Form1 : Form
+    {
+
+
+        PictureBox[] picCard = new PictureBox[100];
+        PictureBox backCard = new PictureBox();
+        int[] cardValue = new int[53];
+        HashSet<int> playerCards = new HashSet<int>();
+        HashSet<int> dealerCards = new HashSet<int>();
+        HashSet<int> usedCards = new HashSet<int>();
+        int playerValue = 0;
+        int dealerValue = 0;
+        int count = 0;
+
+
+        //location vars
+
+        int xPlayerLocation = 50;
+        int yPlayerLocation = 200;
+        int xDealerLocation = 50;
+        int yDealerLocation = 65;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            //music for new game
+            Console.Beep(300, 100);
+            Console.Beep(450, 200);
+            
+           
+
+
+
+            //reset buttons and stuff
+            resetPosition();
+            btnDeal.Enabled = true;
+            count = 0;
+            
+
+            // create back of card
+
+            backCard = new PictureBox();
+            backCard.Location = new Point(-100, 100); // putting a negative value makes it invisible without having to Visible = false;
+            backCard.SizeMode = PictureBoxSizeMode.AutoSize;
+            backCard.Image = Image.FromFile("backofcard.bmp");
+            this.Controls.Add(backCard);
+
+
+
+
+            // create other cards
+
+            for (int i = 1; i < 53; i++)
+            {
+
+                picCard[i] = new PictureBox();
+                picCard[i].Location = new Point(-100, 100); // putting a negative value makes it invisible without having to Visible = false;
+                picCard[i].SizeMode = PictureBoxSizeMode.AutoSize;
+                picCard[i].Image = Image.FromFile("card" + i.ToString() + ".bmp");
+                this.Controls.Add(picCard[i]);
+
+               
+
+            }
+
+
+            //assigning original value to cards
+
+            for (int i = 2; i < 11; i++)  // diamonds
+            {
+                cardValue[i] = i;
+
+            }
+            for (int i = 15; i < 24; i++) // clubs
+            {
+                cardValue[i] = i - 13;
+
+            }
+            for (int i = 28; i < 37; i++) //hearts
+            {
+                cardValue[i] = i - 26;
+
+            }
+            for (int i = 41; i < 50; i++) //spades
+            {
+                cardValue[i] = i - 39;
+
+            }
+
+            cardValue[1] = 11; //default
+            cardValue[14] = 11;
+            cardValue[27] = 11;
+            cardValue[40] = 11;
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                cardValue[11 + i * 13] = 10;
+                cardValue[12 + i * 13] = 10;
+                cardValue[13 + i * 13] = 10;
+
+            }
+
+            //This function resets all the cards
+            
+            playerCards.Clear();
+            dealerCards.Clear();
+            usedCards.Clear();
+
+            //update positions
+            updateScore();
+            updatePosition();
+            
+
+
+
+            btnReset.Enabled = false;
+            backCard.Location = new Point(-100, 100);
+
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            Console.Beep(300, 333);
+
+            while (true)
+            {
+                Random rnd = new Random();
+                int RandomCard = rnd.Next(1, 53);
+
+                
+
+                if (!usedCards.Contains(RandomCard))
+                {
+                    playerCards.Add(RandomCard);
+                    usedCards.Add(RandomCard);
+                }
+
+
+                if (playerCards.Count() == 3 + count)
+                {
+                    count++;
+                    break;
+                }
+
+
+
+            }
+            updatePosition();
+            updateScore();
+            checkBust();
+
+           
+
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            Console.Beep(300, 333);
+
+            while (true)
+            {
+                updateScore();
+
+                if (dealerValue > 20 || dealerValue > playerValue)
+                {
+                    break;
+                }
+
+               
+                Random rnd = new Random();
+                int RandomCard = rnd.Next(1, 53);
+
+                if (!usedCards.Contains(RandomCard))
+                {
+                    dealerCards.Add(RandomCard);
+                    usedCards.Add(RandomCard);
+                }
+
+               
+
+
+            }
+            
+
+            checkWin();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) { 
+
+            
+            
+            
+                Console.Beep(200, 333);
+                Console.Beep(222, 200);
+                Console.Beep(222, 150);
+
+
+
+            btnDeal.Enabled = false;
+            btnHit.Enabled = true;
+            btnStand.Enabled = true;
+
+            // DEAL PLAYER CARDS
+
+            while (true)
+            {
+
+                Random rnd = new Random();
+                int RandomCard = rnd.Next(1, 53);
+
+
+                if (!usedCards.Contains(RandomCard))
+                {
+                    playerCards.Add(RandomCard);
+                    usedCards.Add(RandomCard);
+                }
+
+                if (playerCards.Count() == 2)
+                {
+                   
+                    break;
+                }
+
+            }
+
+            // DEAL DEALER CARDS
+            while (true)
+            {
+                Random rnd = new Random();
+                int RandomCard = rnd.Next(1, 53);
+
+
+
+                if (!usedCards.Contains(RandomCard))
+                {
+                    dealerCards.Add(RandomCard);
+                    usedCards.Add(RandomCard);
+                }
+
+
+                if (dealerCards.Count() == 2)
+                {
+                    
+                    break;
+                }
+
+            }
+
+            updatePosition();
+            updateScore();
+            blackJack(); // checks if it's an instant win or not
+        }
+
+        // my methods
+
+
+        void updateScore()
+        {
+
+            playerValue = 0;
+            dealerValue = 0;
+            int playerCountAce = 0;
+            int dealerCountAce = 0;
+
+
+           
+            for (int i = 1; i < 53; i++)
+            {
+                if (playerCards.Contains(i))
+                {
+                    playerValue = playerValue + cardValue[i];
+                }
+            }
+
+
+            //counts how many aces you have 
+
+            if (playerCards.Contains(1))
+            {
+                playerCountAce++;
+            }
+            if (playerCards.Contains(14))
+            {
+                playerCountAce++;
+            }
+
+            if (playerCards.Contains(27))
+            {
+                playerCountAce++;
+            }
+
+            if (playerCards.Contains(40))
+            {
+                playerCountAce++; 
+            }
+
+ 
+
+
+            if (playerCountAce > 0 && playerValue > 21)
+            {
+
+                playerValue = playerValue - 10 * playerCountAce;
+
+            }
+            //////////////////
+            for (int i = 1; i < 53; i++)
+            {
+                if (dealerCards.Contains(i))
+                {
+                    dealerValue = dealerValue + cardValue[i];
+                }
+            }
+
+
+            //counts how many aces you have 
+
+            if (dealerCards.Contains(1))
+            {
+                dealerCountAce++;
+            }
+            if (dealerCards.Contains(14))
+            {
+                dealerCountAce++;
+            }
+
+            if (dealerCards.Contains(27))
+            {
+                dealerCountAce++;
+            }
+
+            if (dealerCards.Contains(40))
+            {
+                dealerCountAce++;
+            }
+
+
+            //subtracts ace values if you're over 21
+
+            if (dealerCountAce > 0 && dealerValue > 21)
+            {
+
+                dealerValue = dealerValue - 10 * dealerCountAce;
+
+            }
+            ////
+
+           
+            lblPlayerValue.Text = "Player Value: " + Convert.ToString(playerValue);
+           
+
+        }
+
+        void resetPosition()
+        {
+            backCard.Visible = false;
+
+            for (int i = 0; i < playerCards.Count(); i++)
+            {
+
+                picCard[playerCards.ElementAt(i)].Visible = false;
+
+
+
+
+            }
+            for (int i = 0; i < dealerCards.Count(); i++)
+            {
+                picCard[dealerCards.ElementAt(i)].Visible = false;
+            }
+
+        }
+
+        void updatePosition()
+        {
+
+            for (int i = 0; i < playerCards.Count(); i++)
+            {
+
+
+                picCard[playerCards.ElementAt(i)].Location = new Point(xPlayerLocation + (i * 10), yPlayerLocation);
+                picCard[playerCards.ElementAt(i)].BringToFront();
+
+
+
+
+            }
+            for (int i = 0; i < dealerCards.Count(); i++)
+            {
+
+                if(i == 0)
+                {
+                   backCard.Location = new Point(xDealerLocation + (i * 10), yDealerLocation);
+                    backCard.BringToFront();
+
+                }
+                else
+                {
+                    picCard[dealerCards.ElementAt(i)].Location = new Point(xDealerLocation + (i * 10), yDealerLocation);
+                    picCard[dealerCards.ElementAt(i)].BringToFront();
+                }
+
+
+                
+            }
+
+
+        }
+
+        void finalPosition()
+        {
+
+            for (int i = 0; i < playerCards.Count(); i++)
+            {
+
+
+                picCard[playerCards.ElementAt(i)].Location = new Point(xPlayerLocation + (i * 10), yPlayerLocation);
+                picCard[playerCards.ElementAt(i)].BringToFront();
+
+
+
+
+            }
+            for (int i = 0; i < dealerCards.Count(); i++)
+            {
+
+             
+     
+                    picCard[dealerCards.ElementAt(i)].Location = new Point(xDealerLocation + (i * 10), yDealerLocation);
+                    picCard[dealerCards.ElementAt(i)].BringToFront();
+
+
+
+
+            }
+
+        }
+
+
+
+        void checkBust()
+        {
+
+            if (playerValue > 21) // player bust
+            {
+                loseScreen();
+               
+
+            }
+
+        }
+
+        void checkWin()
+        {
+
+            if (dealerValue > 21) // dealer bust
+            {
+                winScreen();
+              
+            }
+
+            if (playerValue > dealerValue) //player has a higher score than the dealer
+            {
+                winScreen();
+                
+            }
+
+
+            if (playerValue < dealerValue && dealerValue <= 21) //player has a lower score than dealer && dealer has not busted
+            {
+                loseScreen();
+                
+            }
+
+            if(dealerValue == playerValue)
+            {
+
+                MessageBox.Show("YOU TIED!!!");
+                finalPosition();
+                disableAllButtons();
+                btnReset.Enabled = true;
+
+            }
+
+        }
+
+        //// Change txt labels nd stuff
+        ///
+
+        void winScreen()
+        {
+            
+            finalPosition();
+            disableAllButtons();
+            MessageBox.Show("YOU WIN!!!");
+            btnReset.Enabled = true;
+            
+            //sound effect happy
+            Console.Beep(261, 233);
+            Console.Beep(329, 233);
+            Console.Beep(392, 233);
+
+
+        }
+        void loseScreen()
+        {
+            
+            finalPosition();
+            disableAllButtons();
+            MessageBox.Show("YOU LOSE!!!");
+            btnReset.Enabled = true;
+
+            //sad sound effect
+            Console.Beep(392, 233);
+            Console.Beep(311, 233);
+            Console.Beep(261, 233);
+
+        }
+
+        void disableAllButtons()
+        {
+            btnDeal.Enabled = false;
+            btnHit.Enabled = false;
+            btnStand.Enabled = false;
+        }
+       
+        void blackJack()
+        {
+
+            if (playerCards.Count == 2) {
+               
+                
+                if (playerCards.Contains(1) && playerValue == 21 || playerCards.Contains(14) && playerValue == 21 
+                    || playerCards.Contains(27) && playerValue == 21 || playerCards.Contains(40) && playerValue == 21
+                    )
+                {
+           
+                       
+                  
+                        winScreen();
+                        
+
+                
+            }
+            
+            
+            }
+
+
+
+
+        }
+        
+
+
+
+
+    }
+}
